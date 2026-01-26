826240a7c21c7387a669966c3256c27e527d82f1
obaidaamer-ing <obaidaamer@uotelafer.edu.iq>
2026-01-26 12:04:21 +0300

    main

---
+++ research/research/doctype/scientific_research_project_approval_form/scientific_research_project_approval_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, obaida and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Scientific Research Project Approval Form", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ research/research/doctype/scientific_research_project_approval_form/test_scientific_research_project_approval_form.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, obaida and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestScientificResearchProjectApprovalForm(FrappeTestCase):
+	pass
